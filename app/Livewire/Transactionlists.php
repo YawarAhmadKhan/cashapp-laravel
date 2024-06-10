8962d4edf7e3da1826f33d2b94baa78a00588be3
Yawar Ahmad Khan <36065624+yawaRcan@users.noreply.github.com>
2024-06-10 19:05:19 +0500

    transaction list added

--- app/Livewire/Transactionlists.php
+++ app/Livewire/Transactionlists.php
@@ -10,11 +10,23 @@ use Livewire\Component;
 
 class Transactionlists extends Component
 {
+    public $search = '';
     use WithPagination;
     #[On('transactioncompleted')]
     public function render()
     {
-        $list = Transaction::orderBy('updated_at', 'desc')->paginate(10);
+        $query = Transaction::orderBy('updated_at', 'desc');
+        if ($this->search) {
+
+            $query->where(function ($subquery) {
+                $subquery->where('amount', 'like', '%' . $this->search . '%')
+                    ->orWhere('notes', 'like', '%' . $this->search . '%')
+                    ->orWhere('transaction_type', 'like', '%' . $this->search . '%');
+            });
+
+            // dd($query->get());
+        }
+        $list = $query->paginate(10);
         return view('livewire.transactionlists', compact('list'));
     }
 }
--- resources/views/livewire/transactionlists.blade.php
+++ resources/views/livewire/transactionlists.blade.php
@@ -7,10 +7,19 @@
                 class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">Transaction Lists</h6>
                 <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
+                     <!-- Topbar Search -->
+    <form
+    class="border border-rounded form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+    <div class="input-group">
+        <input type="text" class="form-control bg-light border-0 small" wire:model.live="search" placeholder="Search for..."
+            aria-label="Search" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+            <button class="btn btn-primary" type="button">
+                <i class="fas fa-search fa-sm"></i>
+            </button>
+        </div>
+    </div>
+</form>
                     
                 </div>
             </div>
